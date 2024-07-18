ab5beeb8fda685b2d0721195972e96ae7a1f61c5
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-07-18 02:23:55 +0000

    :game_die: Nuestro primer dado!

--- inmediatetly.js
+++
@@ -1,8 +0,0 @@
-// IIFE
-
-// Inmediatly Invoke Function Expresion
-
-(() => {
-    console.log("Hola Latam PT 26 😲")
-    console.log("Testing IIFE")
-})();
--- math.js
+++
@@ -1,7 +0,0 @@
-let amigos = [ 'Luis', 'Enzo', 'Valentina M.', 'Alexander', 'Jean' ]
-
-let numerito =  Math.random() * amigos.length
-
-let numRound = Math.floor(numerito)
-
-console.log( amigos[numRound] )
